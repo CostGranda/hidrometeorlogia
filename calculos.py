8b12a43780d7394b6affaba10e3d53ed97edc2ac
Cost Granda <jorge_granda82112@elpoli.edu.co>
2017-10-24 17:46:05 -0500

    funcion documentada

--- calculos.py
+++ calculos.py
@@ -6,11 +6,17 @@ Created on Tue Oct 24 16:54:30 2017
 """
 from datetime import date
 
-def calcularFecha(fecha):
-    """Recibe la fecha en formato d/m/y"""
-    FECHA_MAX = date(2017,6,29)
+def calcularFecha(fecha='29/6/2017'):
+    """
+    Recibe la fecha en formato 'd/m/y' y le resta la fecha mínima (1/1/1998) de los datos.
+    Si se llama sin argumentos toma por defecto la última fecha permitida.
+    Retorna el día de tipo entero.
+    """
     FECHA_MIN = date(1998,1,1)
+    # Al hacer el split los elementos son de tipo string
     fecha_str = fecha.split('/')
-    fecha_int = list(map(int, fecha_str))
-    dias = date(fecha_int[2],fecha_int[1],fecha_int[0])-FECHA_MIN
+    # Convierte a enteros todos los elementos de la lista
+    fecha_int = list(map(int, fecha_str)) 
+    # Se envián en orden año, mes y día
+    dias = date(fecha_int[2], fecha_int[1], fecha_int[0]) - FECHA_MIN
     return dias.days
