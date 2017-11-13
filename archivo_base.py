8827f1e914f188739da84900b5048fde69b457f5
Cost Granda <jorge_granda82112@elpoli.edu.co>
2017-11-12 20:42:57 -0500

    new file

--- archivo_base.py
+++ archivo_base.py
@@ -9,30 +9,80 @@ from mpl_toolkits.basemap import Basemap
 from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 from calculos import calcularFecha
+from openpyxl import load_workbook
+from datetime import date
+import numpy as np
+
+
+libro = load_workbook('C:\\Users\\larom\\Google Drive\\MAESTRIA RRHH\\HIDROMETEOROLOGIA\\Registros DAPARD\\Ficha de Reporte_Emergencias_2014_Final.xlsx')
+hoja = libro.get_sheet_by_name('coordenadas')
+
+def coordMunicipios():   
+    municipios = {}
+    seleccion = hoja['A1:B125']    
+    for linea in seleccion:
+        (municipio, coordenada) =  linea
+        municipios[municipio.value] = coordenada.value.split(', ')
+        variable = list(map(float, municipios[municipio.value]))
+        variable = list(map(lambda x: x/0.25, variable))
+        variable[0] = int(1440 + variable[0])
+        variable[1] = int(variable[1] + 200)
+        variable[2] = int(1440 + variable[2])
+        variable[3] = int(variable[3] + 200)
+        municipios[municipio.value] = variable
+
+    return municipios
+
+
 
 # Open dataset
 dataset = Dataset('http://apdrc.soest.hawaii.edu:80/dods/public_data/satellite_product/TRMM/TRMM_PR/3B42_daily/v7')
-#La lat/lon empieza en 0, uno menos del de la tabla.
-# Longitud: 77.13W = 1132, 74.13W = 1144, avanzan de a .25
+#La lat/lon empieza en 0, uno menos del de la tabla de la página.
+# Longitud: 77.13W = 1132, 74.63W = 1146, avanzan de a .25
 # Latitud: 8.92N = 236, 5,32N  = 222
-data = dataset.variables['precipitation'][calcularFecha('29/6/2017'),221:236,1131:1144]
 
-plt.figure(figsize=(10,10))
-#6.216667, -75.566667
-m = Basemap(projection='lcc', resolution='h',epsg=3115,llcrnrlat=5.375,llcrnrlon=282.625,urcrnrlat=8.92,urcrnrlon=286.625)
+matriz = np.zeros((1155,400,1440))
+data = dataset.variables['hqprecipitation'][calcularFecha('1/1/2014'):calcularFecha('1/3/2017'),:,:]
+matriz = data
 
-m.drawcoastlines()
-m.drawstates()
-#m.drawparallels(np.arange(m.latmin, m.latmax, 0.5), labels=[1,0,0,0])
-#m.drawmeridians(np.arange(m.lonmin, m.lonmax, 0.5), labels=[0,0,0,1])
+def precipitacion(municipio, fecha):
+    prom = []
+    coordenadas = coordMunicipios()
+    west = coordenadas[municipio][0]
+    south = coordenadas[municipio][1]
+    east = coordenadas[municipio][2]
+    north = coordenadas[municipio][3]
+    final = calcularFecha(fecha)
+    inicial = final - 10
+    for dia in range(inicial, final+1):
+        matriz = data[dia,south:north,west:east]
+        prom.append()
+    
+        
+        
+    
 
-ny = data.shape[0]; nx = data.shape[1]
-lons, lats = m.makegrid(nx, ny)
-x, y = m(lons, lats)
+#plt.figure()
+#
+###6.216667, -75.566667
+#m = Basemap(projection='cyl', resolution='h', lat_0=6.216667, lon_0=-75.566667, llcrnrlat=5.38,llcrnrlon=282.745,urcrnrlat=8.88,urcrnrlon=286.245)
+#
+#m.drawcoastlines()
+#m.drawstates()
+##m.drawparallels(np.arange(m.latmin, m.latmax, 0.5), labels=[1,0,0,0])
+##m.drawmeridians(np.arange(m.lonmin, m.lonmax, 0.5), labels=[0,0,0,1])
+#
+#ny = data.shape[0]; nx = data.shape[1]
+#lons, lats = m.makegrid(nx, ny)
+#x, y = m(lons, lats)
+#
+#cs = m.contourf(x,y,data, cmap = plt.get_cmap('RdYlGn_r'))
+#cbar = m.colorbar(cs,location='bottom', pad='5%')
+#cbar.set_label('mm')
+#plt.title('Precipitación')
+##plt.savefig('C:\\Users\\larom\\Documents\\GitHub\\hidrometeorlogia\\hola.png')
+#plt.show()
 
-cs = m.contourf(x,y,data,7,cmap = plt.get_cmap('spectral'))
-cbar = m.colorbar(cs,location='bottom', pad='20%')
-cbar.set_label('mm')
-plt.title('Precipitation')
-plt.savefig('C:\\Users\\larom\\Documents\\GitHub\\hidrometeorlogia\\hola.png')
-plt.show()
+
+plt.figure()
+plt.plot(['lunes','enero','miercoles','jueves','febrero'], [1,2,3,4,5],'ro')
--- calculos.py
+++ calculos.py
@@ -12,7 +12,7 @@ def calcularFecha(fecha='29/6/2017'):
     Si se llama sin argumentos toma por defecto la última fecha permitida.
     Retorna el día de tipo entero.
     """
-    FECHA_MIN = date(1998,1,1)
+    FECHA_MIN = date(2014,1,1)
     # Al hacer el split los elementos son de tipo string
     fecha_str = fecha.split('/')
     # Convierte a enteros todos los elementos de la lista
